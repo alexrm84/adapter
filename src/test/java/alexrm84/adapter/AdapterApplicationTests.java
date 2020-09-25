457a35a1a9aa16aa1b723a815e2758ae04d18373
alexrm84 <alexrm84@gmail.com>
2020-09-25 14:31:39 +0300

    Initial

---
+++ src/test/java/alexrm84/adapter/AdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package alexrm84.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
